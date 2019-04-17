c98f51a8912383cef22496cbac015f86a92320f5
Grégoire Joncour <greg.joncour@gmail.com>
2019-04-18 01:04:49 +0200

    Cars Model

---
+++ LadaStore/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LadaStore.Models
+{
+    public class Car : BaseModel
+    {
+        [Required]
+        public string CarType { get; set; }
+
+        [ForeignKey("User")]
+        public string UserID { get; set; }
+
+        [ForeignKey("CarModels")]
+        public string CarModelID { get; set; }
+
+        [Required]
+        public string Year { get; set; }
+
+        [Required]
+        public double Kilometers { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Picture { get; set; }
+    }
+}
