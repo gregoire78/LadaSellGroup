25533c959fa0f6d2dbd07414226248d775fc3c04
lvanPeric <ivan.peric93100@gmail.com>
2019-04-19 17:04:23 +0200

    authors.txt et affichage du profil

---
+++ LadaStore/Controllers/ProfileController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LadaStore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LadaStore.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Identity;
+
+namespace LadaStore.Controllers
+{
+    public class ProfileController : BaseController
+    {
+        private readonly UserManager<User> userManager;
+
+        public ProfileController(UserManager<User> userManager, LadaDbContext context) : base(context)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Profile/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
