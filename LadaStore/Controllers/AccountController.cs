bb46eeb033e894360ed833b8ddd5643fcd1f51a0
Grégoire <greg.joncour@gmail.com>
2019-04-19 11:30:45 +0200

    register form

---
+++ LadaStore/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LadaStore.Data;
+using LadaStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LadaStore.Controllers
+{
+    public class AccountController : BaseController
+    {
+        private readonly SignInManager<User> signInManager;
+        private readonly UserManager<User> userManager;
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager, LadaDbContext context) : base(context)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            /*if (DateTime.Now.AddYears(-18) <= model.BirthDate)
+                ModelState.AddModelError("BirthDate", "Vous devez avoir plus de 18 ans.");*/
+
+            if (ModelState.IsValid)
+            {
+                var user = new User
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Lastname = model.Name,
+                    Firstname = model.Firstname,
+                    IsCustomer = true,
+                    Address = model.Address,
+                    Siret = model.Siret
+                };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", result.ToString());
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ LadaStore/Models/RegisterViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LadaStore.Models
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Nom", Prompt = "votre nom")]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        public string Name { get; set; }
+
+        [Display(Name = "Prénom", Prompt = "votre prénom")]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Adresse email", Prompt = "exemple@exemple.com")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        [EmailAddress(ErrorMessage = "{0} n'est pas au bon format.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Mot de passe", Prompt = "password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        //[RegularExpression("expression", ErrorMessage = "")]
+        [StringLength(18, MinimumLength = 6, ErrorMessage = "{0} doit contenir entre {2} et {1} caractères.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirmation mot de passe", Prompt = "password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "{0} n'est identique à {1}.")]
+        public string ConfirmedPassword { get; set; }
+
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "{0} obligatoire")]
+        [DataType(DataType.Text)]
+        public String Address { get; set; }
+
+        [Display(Name="Siret")]
+        [DataType(DataType.Text)]
+        public string Siret { get; set; }
+
+    }
+}
