d853a6e5a88abae8c323e78169d1d11a575a8aa9
Grégoire <greg.joncour@gmail.com>
2019-04-19 17:00:53 +0200

    add ing car start

---
+++ LadaStore/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LadaStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LadaStore.Controllers
+{
+    public class CarController : BaseController
+    {
+        public CarController(LadaDbContext context) : base(context)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewData["CarModelID"] = new SelectList(_context.CarModels, "ID", "ModelName");
+            return View();
+        }
+    }
+}
