a22a189b720b28fc27c30ae87576be26b60248f3
Grégoire Joncour <greg.joncour@gmail.com>
2019-04-23 22:40:33 +0200

    add a car

--- LadaStore/Controllers/CarController.cs
+++ LadaStore/Controllers/CarController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LadaStore.Data;
+using LadaStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace LadaStore.Controllers
 {
@@ -14,11 +16,26 @@ namespace LadaStore.Controllers
         {
         }
 
+
         [HttpGet]
         public IActionResult Add()
         {
             ViewData["CarModelID"] = new SelectList(_context.CarModels, "ID", "ModelName");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add([Bind("CarType,CarModelID,Year,Kilometers,Description,Picture")] Car car )
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(car);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(HomeController.Index));
+            }
+            ViewData["CarModelID"] = new SelectList(_context.CarModels, "ID", "ModelName", car.CarModelID);
+            return View(car);
         }
     }
 }
